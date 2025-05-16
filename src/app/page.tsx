6864f72980f3d996be4a708983bee637a1fffe45
Vaibhav P <vaibhav.martian@gmail.com>
2025-05-16 10:15:42 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
       <section
         className="p-6 pb-12 h-auto lg:h-full w-full bg-primary flex gap-4"
       >
-        <Link href="https://drive.google.com/file/d/1D1vjTmtsbNaJL4qR2OXHLsFn-zAD_9IM/view?usp=sharing" className="flex">
+        <Link target="_blank" href="https://drive.google.com/file/d/1D1vjTmtsbNaJL4qR2OXHLsFn-zAD_9IM/view?usp=sharing" className="flex">
           <p className="text-[1.5rem] hover:underline text-gray-300">resume</p><ArrowTopRightIcon className="size-5" />
         </Link>
-        <Link href="https://www.youtube.com/@_martiandev" className="flex">
+        <Link target="_blank" href="https://www.youtube.com/@_martiandev" className="flex">
           <p className="text-[1.5rem] hover:underline text-gray-300">youtube</p><ArrowTopRightIcon className="size-5" />
         </Link>
-        <Link href="https://www.kaggle.com/martian7/code" className="flex">
+        <Link target="_blank" href="https://www.kaggle.com/martian7/code" className="flex">
           <p className="text-[1.5rem] hover:underline text-gray-300">kaggle</p><ArrowTopRightIcon className="size-5" />
         </Link>
       </section>
