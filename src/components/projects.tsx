eee4157f6e72d82ba9d84a89fd8ffaea4d81581b
Vaibhav P <vaibhav.martian@gmail.com>
2025-05-29 21:16:27 +0530

    minor fix in the hyprlink styling in project section

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,23 +5,23 @@ interface Project {
   name: string;
   url: string;
   desc: string;
-};
+}
 
 const projects: Project[] = [
   {
     name: "zen",
     desc: "my implementation of how a frictionless task scheduler is supposed to be.",
-    url: "https://github.com/Martian-dev/zen"
+    url: "https://github.com/Martian-dev/zen",
   },
   {
     name: "tic-tac-toe",
     url: "https://github.com/Martian-dev/TicTacToe",
-    desc: "multiplayer tictactoe game."
+    desc: "multiplayer tictactoe game.",
   },
   {
     name: "dash",
     url: "https://github.com/Martian-dev/dash",
-    desc: "posix compliant shell written using c++."
+    desc: "posix compliant shell written using c++.",
   },
   {
     name: "protein.sh",
@@ -31,20 +31,15 @@ const projects: Project[] = [
   {
     name: "ace",
     url: "https://github.com/Martian-dev/ace",
-    desc: "yes another task management app, but this time aimed at teams and organizations"
+    desc: "yes another task management app, but this time aimed at teams and organizations",
   },
 ];
 
 export default function Projects() {
   return (
-    <section
-      className="p-6 h-auto lg:h-full w-full bg-primary"
-      id="projects"
-    >
+    <section className="p-6 h-auto lg:h-full w-full bg-primary" id="projects">
       <div className="h-full w-full leading-8">
-        <Link
-          href="#projects"
-        >
+        <Link href="#projects">
           <h1
             className={`${fira_code.className} text-[2rem] font-semibold text-fg-secondary hover:underline`}
           >
@@ -53,23 +48,34 @@ export default function Projects() {
         </Link>
         <ul className="text-[1.2rem] py-3">
           <p>these are my ongoing projects</p>
-          {
-            projects.map(({ name, url, desc }: Project, i) => (
-              <li key={i} className="py-2">
-                <Link
-                  href={url}
-                  className="text-fg-accent hover:underline underline-offset-2 font-medium"
-                >
-                  {`${name}: `}
-                </Link>
-                <p className="inline">
-                  {desc}
-                </p>
-              </li>
-            ))
-          }
+          {projects.map(({ name, url, desc }: Project, i) => (
+            <li key={i} className="py-2">
+              <Link
+                href={url}
+                className="text-fg-accent hover:underline underline-offset-2 font-medium"
+              >
+                {`${name}: `}
+              </Link>
+              <p className="inline">{desc}</p>
+            </li>
+          ))}
         </ul>
-        <p className="text-[1.2rem] py-3">there are lots of other projects, you can find them in my <Link className="text-gray-300 hover:underlined" href="https://github.com/Martian-dev">github</Link> and my <Link className="text-gray-300 hover:underlined" href="https://www.kaggle.com/martian7/code">kaggle.</Link></p>
+        <p className="text-[1.2rem] py-3">
+          there are lots of other projects, you can find them in my{" "}
+          <Link
+            className="text-gray-300 font-bold underline hover:bg-gray-300 hover:text-[#262427]"
+            href="https://github.com/Martian-dev"
+          >
+            github
+          </Link>{" "}
+          and my{" "}
+          <Link
+            className="text-gray-300 hover:bg-gray-300 hover:text-[#262427] font-bold"
+            href="https://www.kaggle.com/martian7/code"
+          >
+            kaggle.
+          </Link>
+        </p>
       </div>
     </section>
   );
